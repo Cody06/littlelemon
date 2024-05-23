96c156915d471f015182ec5891a0b001d984abb4
Cody06 <codydev22@gmail.com>
2024-05-23 17:07:01 -0400

    Add rest framework token authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 
@@ -8,6 +9,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
